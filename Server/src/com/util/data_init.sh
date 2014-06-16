8d4844834e9f9299aeb7fce905a49224a47e5fcb
juyingnan <bunny_gg@hotmail.com>
2014-06-16 17:15:14 +0800

    Create data_init.sh

---
+++ Server/src/com/util/data_init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 数据节点
+
+# 0. 如果已经存在MySql，需要先删除
+
+# 1. install MySql Cluster
+
+# 1.1 extract
+
+# 1.2 move
+
+# 1.3 chown & chgrp
+
+# 1.4 install
+
+# 1.5 auto start config
+
+# 2. mysql config
+
+# 2.1 my.cnf
