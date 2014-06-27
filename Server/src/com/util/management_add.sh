d426866553476e299e685d42b16189ecc355f548
juyingnan <bunny_gg@hotmail.com>
2014-06-28 07:36:34 +0800

    Update management_add.sh

--- Server/src/com/util/management_add.sh
+++ Server/src/com/util/management_add.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # 管理节点
 
-# update new node
+echo "LOCALIP=$1"
+echo "LOCALUSERNAME=$2"
+echo "LOCALPASSWD=$3"
+echo "REMOTEIP=$4"
+echo "REMOTEUSERNAME=$5"
+echo "REMOTEPASSWD=$6"
 
-# restart
+PASSWD=$3
+
+NUM=`/usr/local/bin/ndb_mgm -e "show;"|grep mysqld|awk '{print $2}' `
+let NDBDID=$NUM*2-2
+/usr/local/mysql/bin/ndb_mgm -e shutdown
+echo $PASSWD | sudo -S sed -i '$d' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$d' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a [ndbd]' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a NodeId='"$NDBDID" /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a hostname='"$4" /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a datadir=/usr/local/mysql/data/' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a [mysqld]' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a hostname='"$4" /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a [mysqld]' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S sed -i '$a [mysqld]' /var/lib/mysql-cluster/config.ini
+
+echo $PASSWD | sudo -S /usr/local/bin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini --initial --ndb-nodeid=1
