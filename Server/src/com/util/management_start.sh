4a66fb271f11bf634422a75784b86dcd7c0bbb9d
juyingnan <bunny_gg@hotmail.com>
2014-06-28 02:27:44 +0800

    Update management_start.sh

--- Server/src/com/util/management_start.sh
+++ Server/src/com/util/management_start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # 管理节点
 
+echo "LOCALIP=$1"
+echo "LOCALUSERNAME=$2"
+echo "LOCALPASSWD=$3"
+PASSWD=$3
+
 # 启动管理节点
+echo $PASSWD | sudo -S /usr/local/bin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini --initial --ndb-nodeid=1
 
 # 启动数据节点
+echo $PASSWD | sudo -S /usr/local/mysql/bin/ndbd --initial
 
 # 启动SQL节点
+echo $PASSWD | sudo -S echo $PASSWD | sudo -S 
