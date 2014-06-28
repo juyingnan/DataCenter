0dda4912ea8ebc52e2eddecfe2f4d67e0db672ee
Yingnan Ju <bunny_gg@hotmail.com>
2014-06-29 04:13:11 +0800

    update shellrunner

--- Server/src/com/util/ShellRunner.java
+++ Server/src/com/util/ShellRunner.java
@@ -5,13 +5,8 @@ import java.io.InputStreamReader;
 
 public class ShellRunner
 {
-	public String runShell(String[] args)
-	{
-		return null;
-	}
-
-	public boolean runShell(Boolean mode, String localIP, String localUserName, String localUserPasswd, String localMysqlPasswd, String remoteIP, String remoteUsername, String remoteUserPasswd,
-			String remoteMysqlPasswd, String dataBases[], String[] tables)
+	private boolean mysqlMasterSyncRunner(Boolean mode, String localIP, String localUserName, String localUserPasswd, String localMysqlPasswd, String remoteIP, String remoteUsername,
+			String remoteUserPasswd, String remoteMysqlPasswd, String dataBases[], String[] tables)
 	{
 		/*
 		 * mode: false master_slave true master_master
@@ -40,6 +35,21 @@ public class ShellRunner
 		shpath = shpath.trim();
 		shpath = shpath.substring(0, shpath.length() - 1);
 		shpath += "'";
+		System.out.println(shpath);
+		return run(shpath);
+	}
+
+	private boolean clusterSyncRunner(Boolean isCopyTar, String filename, String localIP, String localUserName, String localUserPasswd, String remoteIP, String remoteUsername, String remoteUserPasswd)
+	{
+		/*
+		 * mode: false master_slave true master_master
+		 */
+		String shpath;
+		shpath = "src/com/util/cluster_caller.sh ";
+		shpath += isCopyTar + " " + filename + " ";
+		shpath += localIP + " " + localUserName + " " + localUserPasswd + " ";
+		shpath += remoteIP + " " + remoteUsername + " " + remoteUserPasswd;
+
 		System.out.println(shpath);
 		return run(shpath);
 	}
@@ -76,13 +86,58 @@ public class ShellRunner
 	public static void main(String[] args)
 	{
 		ShellRunner shellRunner = new ShellRunner();
+		/*
+		 * Master - Master / Slave Sync Test
+		 */
+		// shellRunner.mysqlMasterSync();
+
+		/*
+		 * Cluster Sync Test
+		 */
+		// parameters
+		boolean isCopyTar = false;
+		String localIP = "";
+		String localUserName = "";
+		String localUserPasswd = "";
+		String remoteIP = "";
+		String remoteUsername = "";
+		String remoteUserPasswd = "";
+
+		// Management Init & Start Test
+		shellRunner.clusterManagementInit(true, localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+
+		// Data Node Add Test
+		shellRunner.clusterDataNodeAdd(true, localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+
+	}
+
+	public void mysqlMasterSync()
+	{
 		String[] databases = new String[1];
 		String[] tables = new String[2];
 		databases[0] = "DATABASE0";
 		tables[0] = "TABLE0";
 		tables[1] = "TABLE1";
 		// boolean result = shellRunner.runShell(true, "datacenter-adm.cloudapp.net", "work", "Sceri123", "root", "datacenter-slv.cloudapp.net", "work", "Sceri123", "root", databases, tables);
-		boolean result = shellRunner.runShell(true, "datacenter-adm.cloudapp.net", "work", "Sceri123", "root", "datacenter-slv.cloudapp.net", "work", "Sceri123", "root", databases, tables);
+		boolean result = mysqlMasterSyncRunner(true, "datacenter-adm.cloudapp.net", "work", "Sceri123", "root", "datacenter-slv.cloudapp.net", "work", "Sceri123", "root", databases, tables);
 		System.out.println(result);
+	}
+
+	public void clusterManagementInit(Boolean isCopyTar, String localIP, String localUserName, String localUserPasswd, String remoteIP, String remoteUsername, String remoteUserPasswd)
+	{
+
+		boolean result = clusterSyncRunner(isCopyTar, "management_init.sh", localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+		result = clusterSyncRunner(isCopyTar, "management_start.sh", localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+		// System.out.println(result);
+	}
+
+	public void clusterDataNodeAdd(Boolean isCopyTar, String localIP, String localUserName, String localUserPasswd, String remoteIP, String remoteUsername, String remoteUserPasswd)
+	{
+
+		boolean result = clusterSyncRunner(isCopyTar, "management_add.sh", localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+		result = clusterSyncRunner(isCopyTar, "data_init.sh", localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+		result = clusterSyncRunner(isCopyTar, "management_start.sh", localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+		result = clusterSyncRunner(isCopyTar, "data_start.sh", localIP, localUserName, localUserPasswd, remoteIP, remoteUsername, remoteUserPasswd);
+		// System.out.println(result);
 	}
 }
