f552a863ca8b9ebe960c981a8e6ae32316b57455
juyingnan <bunny_gg@hotmail.com>
2014-07-18 15:15:49 +0800

    Update management_start.sh

--- Server/src/com/util/management_start.sh
+++ Server/src/com/util/management_start.sh
@@ -8,12 +8,15 @@ PASSWD=$3
 
 # 启动管理节点
 echo $PASSWD | sudo -S /usr/local/bin/ndb_mgmd -f /var/lib/mysql-cluster/config.ini --initial --ndb-nodeid=1
+sleep 10s
 
 # 启动数据节点
 echo $PASSWD | sudo -S /usr/local/mysql/bin/ndbd --initial
+sleep 20s
 
 # 启动SQL节点
 echo $PASSWD | sudo -S service mysqld start
+sleep 30s
 
 # 返回结果
 RESULT=`/usr/local/bin/ndb_mgm -e "show;" | grep -c "$1" `
