96099f2ec774fff8368b7f747123a3218b75bd5d
juyingnan <bunny_gg@hotmail.com>
2014-07-18 14:47:10 +0800

    Update management_init.sh

--- Server/src/com/util/management_init.sh
+++ Server/src/com/util/management_init.sh
@@ -15,11 +15,11 @@ cd /tmp
 # 1.1 extract
 tar -zxvf mysql-cluster-gpl-7.3.5-linux-glibc2.5-x86_64.tar.gz
 # 1.2 move
-echo $PASSWD | sudo -S mv -r ./mysql-cluster-gpl-7.3.5-linux-glibc2.5-x86_64 /usr/local/mysql
+echo $PASSWD | sudo -S mv ./mysql-cluster-gpl-7.3.5-linux-glibc2.5-x86_64 /usr/local/mysql
 # 1.3 chown & chgrp
 cd /usr/local/mysql
 echo $PASSWD | sudo -S addgroup mysql
-echo $PASSWD | sudo -S adduser --ingroup mysql mysql
+echo $PASSWD | sudo -S useradd -g mysql mysql
 echo $PASSWD | sudo -S chown -R root .
 echo $PASSWD | sudo -S chown -R mysql ./data
 echo $PASSWD | sudo -S chgrp -R mysql .
@@ -37,6 +37,8 @@ else
     echo "/etc/profile needs changes"
     echo $PASSWD | sudo -S sed -i '$a PATH=$PATH:/usr/local/mysql/bin' /etc/profile
     echo $PASSWD | sudo -S sed -i '$a export PATH' /etc/profile
+    echo $PASSWD | sudo -S PATH=$PATH:/usr/local/mysql/bin
+    echo $PASSWD | sudo -S export PATH
 fi 
 echo $PATH
 # 2. mysql config
@@ -44,7 +46,7 @@ echo $PASSWD | sudo -S cp /usr/local/mysql/bin/ndb_mgm* /usr/local/bin
 echo $PASSWD | sudo -S mkdir /var/lib/mysql-cluster
 # 2.1 config.ini "/var/lib/mysql-cluster/config.ini"
 echo $PASSWD | sudo -S touch /var/lib/mysql-cluster/config.ini
-echo $PASSWD | sudo -S chmod 644 /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S chmod 666 /var/lib/mysql-cluster/config.ini
 echo $PASSWD | sudo -S echo "" >/var/lib/mysql-cluster/config.ini
 echo $PASSWD | sudo -S sed -i '$a [ndbd default]' /var/lib/mysql-cluster/config.ini
 echo $PASSWD | sudo -S sed -i '1d' /var/lib/mysql-cluster/config.ini
@@ -63,9 +65,10 @@ echo $PASSWD | sudo -S sed -i '$a [mysqld]' /var/lib/mysql-cluster/config.ini
 echo $PASSWD | sudo -S sed -i '$a hostname='"$1" /var/lib/mysql-cluster/config.ini
 echo $PASSWD | sudo -S sed -i '$a [mysqld]' /var/lib/mysql-cluster/config.ini
 echo $PASSWD | sudo -S sed -i '$a [mysqld]' /var/lib/mysql-cluster/config.ini
+echo $PASSWD | sudo -S chmod 644 /var/lib/mysql-cluster/config.ini
 # 2.2 my.cnf
 echo $PASSWD | sudo -S touch /etc/my.cnf
-echo $PASSWD | sudo -S chmod 644 /etc/my.cnf
+echo $PASSWD | sudo -S chmod 666 /etc/my.cnf
 echo $PASSWD | sudo -S echo "" >/etc/my.cnf
 echo $PASSWD | sudo -S sed -i '$a [mysqld]' /etc/my.cnf
 echo $PASSWD | sudo -S sed -i '1d' /etc/my.cnf
@@ -75,3 +78,4 @@ echo $PASSWD | sudo -S sed -i '$a basedir=/usr/local/mysql' /etc/my.cnf
 echo $PASSWD | sudo -S sed -i '$a port=3306' /etc/my.cnf
 echo $PASSWD | sudo -S sed -i '$a [mysql_cluster]' /etc/my.cnf
 echo $PASSWD | sudo -S sed -i '$a ndb-connectstring='"$1" /etc/my.cnf
+echo $PASSWD | sudo -S chmod 644 /etc/my.cnf
